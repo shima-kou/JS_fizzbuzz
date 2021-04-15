b59e69405d70643a3fd6c1b9aa316209e2af102d
shima-kou <wakeup1991kou@gmail.com>
2021-04-15 23:14:37 +0900

    そのほか修正いたしました。

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
-const fizzbuzz = () => {
-  const fizzNum = Number(document.getElementById("FizzNum").value);
-  const buzzNum = Number(document.getElementById("BuzzNum").value);
-  const fizzBuzzArray = [];
-  const fizzBuzzText = [];
-  const fizzBuzzResult = document.getElementById("fizzbuzz-result");
+const createFizzBuzz = () => {
+  const fizzNum = Number(document.getElementById('FizzNum').value);
+  const buzzNum = Number(document.getElementById('BuzzNum').value);
+  const fizzBuzzResult = document.getElementById('fizzbuzz-result');
 
   if (!Number.isInteger(fizzNum) || !Number.isInteger(buzzNum) || fizzNum === 0 || buzzNum === 0) {
-    fizzBuzzResult.textContent = "整数値を入力してください。";
+    fizzBuzzResult.innerHTML = '整数値を入力してください。';
     return false;
   } else if (fizzNum > 99 || buzzNum > 99) {
-    fizzBuzzResult.textContent = "2桁以下の整数で入力してください。";
+    fizzBuzzResult.innerHTML = '2桁以下の整数で入力してください。';
     return false;
   }
 
+  fizzBuzzResult.innerHTML = '';
   for (let i = 1; i < 100; i++) {
     if (i % fizzNum === 0 && i % buzzNum === 0) {
-      fizzBuzzArray.push("FizzBuzz" + i);
+      fizzBuzzResult.insertAdjacentHTML('beforeend', '<p>' + 'FizzBuzz' + i + '</p>');
     } else if (i % fizzNum === 0) {
-      fizzBuzzArray.push("Fizz" + i);
+      fizzBuzzResult.insertAdjacentHTML('beforeend', '<p>' + 'Fizz' + i + '</p>');
     } else if (i % buzzNum === 0) {
-      fizzBuzzArray.push("Buzz" + i);
+      fizzBuzzResult.insertAdjacentHTML('beforeend', '<p>' + 'Buzz' + i + '</p>');
     }
   }
-
-  for (const v of fizzBuzzArray) {
-    fizzBuzzText.push("<p>" + v + "</p>");
-  }
-
-  const fizzBuzzHTML = fizzBuzzText.join("");
-
-  fizzBuzzResult.innerHTML = fizzBuzzHTML;
 };
 
-const actionButton = document.getElementById("actionButton");
-actionButton.addEventListener("click", fizzbuzz);
+const actionButton = document.getElementById('actionButton');
+actionButton.addEventListener('click', createFizzBuzz);
